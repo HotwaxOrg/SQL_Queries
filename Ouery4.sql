de7c233a0196168a7b28b8ab423a8734106b96a1
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-10 10:10:57 +0530

    Modification in 4th query

--- Ouery4.sql
+++ Ouery4.sql
@@ -1,10 +1,8 @@
 -- Selecting the attributes 
 SELECT 
-    product.PRODUCT_ID, 
+    gi.PRODUCT_ID, 
     gi.GOOD_IDENTIFICATION_TYPE_ID, 
     gi.ID_VALUE  
-FROM product  
--- Applying join on the entities
-JOIN good_identification gi ON gi.PRODUCT_ID = product.PRODUCT_ID  
+FROM good_identification gi   
 -- Filtering out records based on required conditions
 WHERE gi.GOOD_IDENTIFICATION_TYPE_ID = 'ERP_ID' or gi.GOOD_IDENTIFICATION_TYPE_ID = 'SHOPIFY_PROD_ID';
