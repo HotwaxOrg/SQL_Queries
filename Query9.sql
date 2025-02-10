e0fb1bad4a83f399f61c56ff8b6d5a7910db3400
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-10 15:25:29 +0530

    Completed query 9 to find the revenue from the BOPIS order

---
+++ Query9.sql
@@ -0,0 +1,5 @@
+select count(oh.ORDER_ID ) as Total_Orders, sum(oh.GRAND_TOTAL) as Revenu from order_header oh 
+join order_item_ship_group oisg on oisg.ORDER_ID = oh.ORDER_ID 
+where  oisg.SHIPMENT_METHOD_TYPE_ID = 'STOREPICKUP' and (oh.SALES_CHANNEL_ENUM_ID = 'CSR_SALES_CHANNEL' or oh.SALES_CHANNEL_ENUM_ID = 'WEB_SALES_CHANNEL');
+
+
