03fb41a01056af090f38983102ca6bc508696031
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-11 15:26:32 +0530

    Query to compare revenue across different facilities

---
+++ SQL-assignment-2/Query4.sql
@@ -0,0 +1,16 @@
+select f.FACILITY_ID , f.FACILITY_TYPE_ID , count(oi.ORDER_ID) Total_Orders, sum(oi.UNIT_PRICE) Total_Revenueor
+,CONCAT(
+        MIN(DATE(oh.ORDER_DATE)), ' to ', MAX(DATE(oh.ORDER_DATE))
+    ) AS Date_Range
+from order_item oi
+join order_header oh on oh.ORDER_ID = oi.ORDER_ID 
+join order_shipment os on os.order_id = oi.order_id
+join shipment s on s.SHIPMENT_ID = os.SHIPMENT_ID 
+join facility f on f.FACILITY_ID = s.ORIGIN_FACILITY_ID 
+group by f.FACILITY_ID
+order by total_orders desc;
+
+
+
+
+desc order_item;
