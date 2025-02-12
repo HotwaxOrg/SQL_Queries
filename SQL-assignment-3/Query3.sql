fdf5ea2d97b6660cacb546d146977e65c4f332c2
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-12 10:40:25 +0530

    Query to get orders with single return

---
+++ SQL-assignment-3/Query3.sql
@@ -0,0 +1,21 @@
+-- Fetching the columns
+select
+	rh.from_party_id ,
+	p2.FIRST_NAME
+from
+	return_header rh
+	-- Getting teh party details 
+	-- Getting the order details
+join return_item ri on
+	ri.RETURN_ID = rh.RETURN_ID
+join party p on
+	p.PARTY_ID = rh.FROM_PARTY_ID
+join person p2 on
+	p2.PARTY_ID = rh.FROM_PARTY_ID
+	-- Filtering out party with single return count
+group by
+	ri.ORDER_ID,
+	rh.from_party_id ,
+	p2.FIRST_NAME
+having
+	count(rh.RETURN_ID) = 1;
