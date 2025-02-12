a390ca97d224f1b8cd370cc82dfade3c7160f753
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-12 14:23:09 +0530

    Query to get the stock of products at virtual facilities

---
+++ SQL-assignment-3/Query10.sql
@@ -0,0 +1,14 @@
+-- Query to get the stock of product in virtual facilities
+select
+	ii.PRODUCT_ID ,
+	ii.FACILITY_ID ,
+	ii.QUANTITY_ON_HAND_TOTAL ,
+	ii.AVAILABLE_TO_PROMISE_TOTAL ,
+	f.FACILITY_TYPE_ID
+from
+	inventory_item ii
+join facility f on
+	f.FACILITY_ID = ii.FACILITY_ID
+where
+	f.FACILITY_TYPE_ID = 'VIRTUAL_FACILITY'
+	or f.FACILITY_TYPE_ID = 'CONFIGURATION';
