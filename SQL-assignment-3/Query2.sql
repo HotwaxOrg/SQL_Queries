16f46ce51ceeac872cc5b16d02fe2411261280ad
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-12 10:26:36 +0530

    Query to get insights of the return items

---
+++ SQL-assignment-3/Query2.sql
@@ -0,0 +1,22 @@
+-- Fetching the columns
+select
+	rh.RETURN_ID ,
+	rh.FROM_PARTY_ID ,
+	rh.ENTRY_DATE ,
+	rh.RETURN_DATE ,
+	rh.RETURN_CHANNEL_ENUM_ID ,
+	ri.ORDER_ID ,
+	rs.STATUS_DATETIME ,
+	oh.PRODUCT_STORE_ID ,
+	oh.ORDER_NAME
+from
+	return_header rh
+	-- Getting the order id
+join return_item ri on
+	ri.RETURN_ID = rh.RETURN_ID
+	-- Getting the store id and order name
+join order_header oh on
+	oh.ORDER_ID = ri.ORDER_ID
+	-- Getting the return status datetime
+join return_status rs on
+	rs.RETURN_ID = rh.RETURN_ID ;
