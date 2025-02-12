2ef0baef0f44f7e6ffbd9df502b36807973efcc2
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-12 11:32:18 +0530

    Query to find order with multiple returns

---
+++ SQL-assignment-3/Query6.sql
@@ -0,0 +1,12 @@
+-- Fetching the columns
+select
+	rh.RETURN_ID ,
+	rh.RETURN_DATE ,
+	ri.ORDER_ID ,
+	ri.RETURN_REASON_ID ,
+	ri.RETURN_QUANTITY
+from
+	return_header rh
+	-- Fetching the order id, return reason id and quantity
+join return_item ri on
+	ri.RETURN_ID = rh.RETURN_ID ;
