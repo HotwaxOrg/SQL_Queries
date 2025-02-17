4df56d60b2e8118e95ed0d76721a72d156f316e5
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-02-17 11:25:15 +0530

    Commited QUery 11 and 12

---
+++ SQL-assignment-3/Query11.sql
@@ -0,0 +1,10 @@
+select
+	it.ORDER_ID ,
+	it.FACILITY_ID ,
+	it.FACILITY_ID_TO ,
+	it.PRODUCT_ID ,
+	it.QUANTITY ,
+	it.SEND_DATE ,
+	it.STATUS_ID
+from
+	inventory_transfer it ;
---
+++ SQL-assignment-3/Query12.sql
@@ -0,0 +1,17 @@
+select
+    os.order_id,
+    oh.order_date,
+    oh.status_id as order_status,
+    s.origin_facility_id as facility_id,
+    ofc.change_datetime as duration
+from order_shipment os
+left join picklist_bin pb
+on pb.primary_order_id = os.ORDER_ID
+join order_header oh 
+on oh.order_id = os.order_id
+join shipment s 
+    on os.shipment_id = s.shipment_id 
+left join order_facility_change ofc 
+    on ofc.order_id = os.order_id
+    where pb.picklist_id is null
+    order by os.order_id;
