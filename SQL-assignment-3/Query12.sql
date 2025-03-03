8cdffaaeec7e9261faef84e5c9af64aba5705b86
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 13:57:01 +0530

    Update Query12.sql

--- SQL-assignment-3/Query12.sql
+++ SQL-assignment-3/Query12.sql
@@ -1,21 +1,24 @@
 select
-    os.order_id,
-    oh.order_date,
-    oh.status_id as order_status,
-    s.origin_facility_id as facility_id,
-    ofc.change_datetime as duration
-from order_shipment os
+	os.order_id,
+	oh.order_date,
+	oh.status_id as order_status,
+	s.origin_facility_id as facility_id,
+	ofc.change_datetime as duration
+from
+	order_shipment os
 left join picklist_bin pb
-on pb.primary_order_id = os.ORDER_ID
+on
+	pb.primary_order_id = os.ORDER_ID
 join order_header oh 
-on oh.order_id = os.order_id
+on
+	oh.order_id = os.order_id
 join shipment s 
-    on os.shipment_id = s.shipment_id 
+    on
+	os.shipment_id = s.shipment_id
 left join order_facility_change ofc 
-    on ofc.order_id = os.order_id
-    where pb.picklist_id is null
-    order by os.order_id;
-
-desc shipment_status;
-
-SELECT  * FROM shipment_status ;
+    on
+	ofc.order_id = os.order_id
+where
+	pb.picklist_id is null
+order by
+	os.order_id;
