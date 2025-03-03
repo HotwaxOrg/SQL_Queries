fb366e1a4caeba668fcb7219cda33cc8fcb8b023
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 15:35:11 +0530

    Update Query4.sql

--- SQL-assignment-2/Query4.sql
+++ SQL-assignment-2/Query4.sql
@@ -1,3 +1,6 @@
+-- Question: Store-Specific (Facility-Wise) Revenue
+
+-- Fetching the attributes
 select
 	f.FACILITY_ID ,
 	f.FACILITY_TYPE_ID ,
@@ -9,15 +12,21 @@ select
     ) as Date_Range
 from
 	order_item oi
+	-- Getting the order details
 join order_header oh on
 	oh.ORDER_ID = oi.ORDER_ID
+	-- Getting the shipment details
 join order_shipment os on
 	os.order_id = oi.order_id
 join shipment s on
 	s.SHIPMENT_ID = os.SHIPMENT_ID
+	-- Getting the facility details
 join facility f on
 	f.FACILITY_ID = s.ORIGIN_FACILITY_ID
 group by
 	f.FACILITY_ID
 order by
 	total_orders desc;
+
+
+-- Query Cost: 262,736.98
