0c3918854973e20235f77e3bf0b968c8e00f7538
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 12:36:15 +0530

    Update assignment2 Query9.sql

--- SQL-assignment-2/Query9.sql
+++ SQL-assignment-2/Query9.sql
@@ -1,3 +1,8 @@
-select oi.ORDER_ID, oi.ORDER_ITEM_GROUP_SEQ_ID , oi.STATUS_ID CurrentStatusID, 
-oi.LAST_UPDATED_STAMP StatusChangeDateTime, oi.CHANGE_BY_USER_LOGIN_ID ChangedBy
-from order_item oi ;
+select
+	oi.ORDER_ID,
+	oi.ORDER_ITEM_GROUP_SEQ_ID ,
+	oi.STATUS_ID CurrentStatusID,
+	oi.LAST_UPDATED_STAMP StatusChangeDateTime,
+	oi.CHANGE_BY_USER_LOGIN_ID ChangedBy
+from
+	order_item oi ;
