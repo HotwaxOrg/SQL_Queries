1b427e6c515e9fe99b3dd77efab50ba3cae35a52
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 15:25:24 +0530

    Update Query9.sql

--- SQL-assignment-1/Query9.sql
+++ SQL-assignment-1/Query9.sql
@@ -1,3 +1,6 @@
+-- Question:  BOPIS Orders Revenue (Last Year)
+
+-- Fetching the attributes
 select
 	count(oh.ORDER_ID) as Total_Orders,
 	sum(oh.GRAND_TOTAL) as Revenu
@@ -8,4 +11,8 @@ join order_item_ship_group oisg on
 where
 	oisg.SHIPMENT_METHOD_TYPE_ID = 'STOREPICKUP'
 	and (oh.SALES_CHANNEL_ENUM_ID = 'CSR_SALES_CHANNEL'
-		or oh.SALES_CHANNEL_ENUM_ID = 'WEB_SALES_CHANNEL');
+		or oh.SALES_CHANNEL_ENUM_ID = 'WEB_SALES_CHANNEL')
+	and oh.ORDER_DATE between '2024-01-01' and '2024-12-31';
+
+
+-- Query Cost: 3,042.95
