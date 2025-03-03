72811a23ee73215c5420368f0664767f79d7d29e
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 11:11:25 +0530

    Update Assignment1 Query2.sql

--- SQL-assignment-1/Query2.sql
+++ SQL-assignment-1/Query2.sql
@@ -1,6 +1,17 @@
 -- Selecting required attributes
-select p.product_id, p.PRODUCT_TYPE_ID, p.INTERNAL_NAME   
-from product p
--- Joining product type and product entity to find physical product
-join product_type pt on pt.PRODUCT_TYPE_ID = p.PRODUCT_TYPE_ID 
-where p.IS_VARIANT = 'y' and pt.IS_PHYSICAL = 'Y';
+select
+	p.PRODUCT_ID,
+	p.PRODUCT_TYPE_ID,
+	p.INTERNAL_NAME
+from
+	PRODUCT p
+	-- Joining product type and product entity to find physical product
+join 
+    PRODUCT_TYPE pt on
+	p.PRODUCT_TYPE_ID = pt.PRODUCT_TYPE_ID
+where
+	pt.IS_PHYSICAL = 'Y'
+	and (p.SALES_DISCONTINUATION_DATE is null
+		or p.SALES_DISCONTINUATION_DATE > NOW())
+	and (p.SUPPORT_DISCONTINUATION_DATE is null
+		or p.SUPPORT_DISCONTINUATION_DATE > NOW());
