e6f0d1a2ae3243c63e4a40f0137bf58942781c7b
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 12:26:02 +0530

    Update assignment2 Query5.sql

--- SQL-assignment-2/Query5.sql
+++ SQL-assignment-2/Query5.sql
@@ -1,15 +1,13 @@
 select
 	iiv.INVENTORY_ITEM_ID ,
 	iiv.VARIANCE_REASON_ID ,
-	iiv.CREATED_TX_STAMP ,
+	iiv.CREATED_TX_STAMP TRANSACTION_DATE,
 	ii.PRODUCT_ID ,
 	ii.FACILITY_ID ,
-	ii.EXPIRE_DATE, 
 	(ii.QUANTITY_ON_HAND_TOTAL + iiv.QUANTITY_ON_HAND_VAR) as Quantity_LOST_or_DAMAGED
 from
 	inventory_item_variance iiv
 join inventory_item ii on
 	ii.INVENTORY_ITEM_ID = iiv.INVENTORY_ITEM_ID;
-
 
 
