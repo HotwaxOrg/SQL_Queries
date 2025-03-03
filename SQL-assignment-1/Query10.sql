2f78def044408a9731a4ccd3b67ec57be3a10a70
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 15:26:25 +0530

    Update Query10.sql

--- SQL-assignment-1/Query10.sql
+++ SQL-assignment-1/Query10.sql
@@ -1,3 +1,6 @@
+-- Question: Canceled Orders (Last Month)
+
+-- Fetching the attributes
 select
 	count(os.STATUS_ID) Total_Orders,
 	os.CHANGE_REASON
@@ -5,3 +8,6 @@ from
 	order_status os
 where
 	os.STATUS_ID = 'ORDER_CANCELLED';
+
+
+-- Query Cost: 9,984.82
