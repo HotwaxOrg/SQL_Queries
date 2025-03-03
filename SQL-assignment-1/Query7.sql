aacc79512a4f7643ae6b85cbfd5d71d691879a11
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 11:31:06 +0530

    Update Assignment1 Query7.sql

--- SQL-assignment-1/Query7.sql
+++ SQL-assignment-1/Query7.sql
@@ -1,6 +1,13 @@
-select oh.ORDER_ID , oh.STATUS_ID, opp.STATUS_ID  , s.STATUS_ID 
-from order_header oh 
-join order_payment_preference opp on opp.ORDER_ID = oh.ORDER_ID 
-join shipment s on s.PRIMARY_ORDER_ID = oh.ORDER_ID 
-where opp.STATUS_ID = 'PAYMENT_SETTLED';
-
+select
+	oh.ORDER_ID ,
+	oh.STATUS_ID,
+	opp.STATUS_ID ,
+	s.STATUS_ID
+from
+	order_header oh
+join order_payment_preference opp on
+	opp.ORDER_ID = oh.ORDER_ID
+join shipment s on
+	s.PRIMARY_ORDER_ID = oh.ORDER_ID
+where
+	opp.STATUS_ID = 'PAYMENT_SETTLED';
