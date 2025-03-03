844513de0fb357dfbda10adca749c50fc82ec78b
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 15:16:17 +0530

    Update Query7.sql

--- SQL-assignment-1/Query7.sql
+++ SQL-assignment-1/Query7.sql
@@ -1,3 +1,6 @@
+-- Question:  Payment Captured but Not Shipped
+
+-- Fetching the attributes
 select
 	oh.ORDER_ID ,
 	oh.STATUS_ID,
@@ -5,9 +8,15 @@ select
 	s.STATUS_ID
 from
 	order_header oh
+	-- Getting the payment details
 join order_payment_preference opp on
 	opp.ORDER_ID = oh.ORDER_ID
+	-- Getting the shipment details
 join shipment s on
 	s.PRIMARY_ORDER_ID = oh.ORDER_ID
+	-- Filtering out records as per our requirements
 where
 	opp.STATUS_ID = 'PAYMENT_SETTLED';
+
+
+-- Query Cost: 44,194.11
