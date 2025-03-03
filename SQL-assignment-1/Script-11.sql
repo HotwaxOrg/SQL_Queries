6b9eef925293e949ca4d489be106a5153930d525
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 11:56:14 +0530

    QUery 12 assignment 1

---
+++ SQL-assignment-1/Script-11.sql
@@ -0,0 +1,15 @@
+select
+	pf.product_id,
+	pf.facility_id,
+	pf.minimum_stock
+from
+	product_facility pf
+join facility f on
+	f.facility_id = pf.facility_id
+where
+	f.FACILITY_TYPE_ID = "CONFIGURATION"
+	and pf.minimum_stock is not null
+group by
+	pf.product_id,
+	pf.facility_id,
+	pf.minimum_stock;
