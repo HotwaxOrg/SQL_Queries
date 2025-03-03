bae3184203ea2370f3ae1b02d53e03fcf5d37873
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 11:15:31 +0530

    Update Assignment1 Query3.sql

--- SQL-assignment-1/Query3.sql
+++ SQL-assignment-1/Query3.sql
@@ -1,11 +1,15 @@
--- Selecting the attributes
-SELECT p.PRODUCT_ID , p.INTERNAL_NAME , p.PRODUCT_TYPE_ID, gi.ID_VALUE  
-FROM product p
--- Applying joins on our entities on product id
-JOIN good_identification gi 
-ON gi.product_id = p.PRODUCT_ID 
--- Filtering our erp id that are null
-where  gi.good_identification_type_id = 'ERP_ID'
-and gi.ID_VALUE IS NULL;
-
-
+-- Selecting the attributes 
+SELECT 
+    p.PRODUCT_ID, 
+    p.INTERNAL_NAME, 
+    p.PRODUCT_TYPE_ID, 
+    gi.ID_VALUE AS NETSUITE_ID
+FROM 
+    PRODUCT p
+    -- Applying joins on our entities on product id
+LEFT JOIN 
+    GOOD_IDENTIFICATION gi ON p.PRODUCT_ID = gi.PRODUCT_ID
+    AND gi.GOOD_IDENTIFICATION_TYPE_ID = 'ERP_ID'
+    -- Filtering our erp id that are null
+WHERE 
+    gi.ID_VALUE IS NULL ;
