ed31223796dd872f874d6ff1b213a3b6fa8c0517
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-03 11:37:56 +0530

    Update Assignment1 Query8.sql

--- SQL-assignment-1/Query8.sql
+++ SQL-assignment-1/Query8.sql
@@ -1,6 +1,12 @@
-SELECT HOUR(os.STATUS_DATETIME) AS Hour, COUNT(os.STATUS_ID) AS Total_Orders
-FROM order_status os
-WHERE os.STATUS_ID = 'ORDER_COMPLETED'
-AND os.STATUS_DATETIME BETWEEN '2023-08-17 00:00:00' AND '2023-08-17 23:59:59'
-GROUP BY HOUR(os.STATUS_DATETIME)
-ORDER BY Hour;
+select
+	COUNT(os.STATUS_ID) as Total_Orders,
+	DATE_FORMAT(os.STATUS_DATETIME, '%H') Hours
+from
+	order_status os
+where
+	os.STATUS_ID = 'ORDER_COMPLETED'
+	and os.STATUS_DATETIME between '2023-08-17 00:00:00' and '2023-08-17 23:59:59'
+group by
+	hour(os.STATUS_DATETIME)
+order by
+	Hours;
