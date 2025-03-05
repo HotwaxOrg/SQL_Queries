86c3ed1b82bb7383ec9dbcabeba1cd0be9171cf0
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-05 18:12:48 +0530

    Update Query6.sql

--- SQL-assignment-3/Query6.sql
+++ SQL-assignment-3/Query6.sql
@@ -1,3 +1,5 @@
+-- Problem Statement:  Orders with Multiple Returns
+
 -- Fetching the columns
 select
 	rh.RETURN_ID ,
@@ -9,4 +11,8 @@ from
 	return_header rh
 	-- Fetching the order id, return reason id and quantity
 join return_item ri on
-	ri.RETURN_ID = rh.RETURN_ID ;
+	ri.RETURN_ID = rh.RETURN_ID
+where
+	ri.RETURN_QUANTITY > 1;
+
+-- Cost: 1,121.71
