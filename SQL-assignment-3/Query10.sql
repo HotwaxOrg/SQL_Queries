5d710a097085622d665711d1855ffbca5b3958dd
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-05 18:24:08 +0530

    Update Query10.sql

--- SQL-assignment-3/Query10.sql
+++ SQL-assignment-3/Query10.sql
@@ -1,3 +1,5 @@
+-- Problem Statement: Total Items in Various Virtual Facilities
+
 -- Query to get the stock of product in virtual facilities
 select
 	ii.PRODUCT_ID ,
@@ -12,3 +14,6 @@ join facility f on
 where
 	f.FACILITY_TYPE_ID = 'VIRTUAL_FACILITY'
 	or f.FACILITY_TYPE_ID = 'CONFIGURATION';
+
+
+-- Cost: 59,409.3
