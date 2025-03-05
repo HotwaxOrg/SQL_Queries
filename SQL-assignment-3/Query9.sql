3560e7ffa55bc30de53f9e5ca127bf52f80521b8
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-03-05 18:21:03 +0530

    Update Query9.sql

--- SQL-assignment-3/Query9.sql
+++ SQL-assignment-3/Query9.sql
@@ -1,3 +1,5 @@
+-- Problem Statement: Total Facilities That Sell the Product
+
 -- Fetching out the columns and counting the number of facilities
 select
 	p.PRODUCT_ID ,
@@ -7,3 +9,6 @@ from
 	product p
 group by
 	p.PRODUCT_ID;
+
+
+-- Cost: 80,133.02
